2e3f9cbec993f249329cb2c97d592b522f748964
Anderson Dantas <andersondantas81@gmail.com>
2019-09-17 17:45:08 -0300

    alteracao

--- src/models/File.js
+++ src/models/File.js
@@ -18,7 +18,7 @@ const File = mongoose.Schema(
   }
 );
 File.virtual("url").get(function() {
-  const url = process.env.URL || "http://localhost:3333";
+  const url = process.env.URL || "http://localhost:3030";
   return `${url}/files/${encodeURIComponent(this.path)}`;
 });
 
